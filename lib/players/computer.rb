eda80ec36f1e9b0d9a1b4732b635e041ff72090c
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-20 02:12:55 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,17 +3,11 @@ module Players
 class Computer < Player
 
 def move(board)
-<<<<<<< HEAD
   move = Random.rand(1...9)
   while(board.taken?(move))
     move = Random.rand(1...9)
   end
   move.to_s
-=======
-  input = rand(0...8)
-  board.valid_move?(input)
-  board.update(input, self.token)
->>>>>>> 6b238377eac3643ab8c2f7dd9032a2ad0c9ba622
 end
 
 end
